2fb43d0fa0347a28ac8fcf1c20119a29ada8f875
ocefpaf <ocefpaf@gmail.com>
2016-06-23 11:48:27 -0300

    Test pin gcc

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -44,6 +44,9 @@ conda install --yes conda-build
 conda info
 
 # Embarking on 1 case(s).
+    set -x
+    export CONDA_PERL=5.20.3.1
+    set +x
     conda build /recipe_root --quiet || exit 1
     /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
 EOF
