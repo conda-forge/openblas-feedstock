756a3824637b38fd38cb9fa39c1ad59d39f51a6b
Isuru Fernando <isuruf@gmail.com>
2017-07-24 22:10:44 +0530

    Fix comment

--- recipe/build.sh
+++ recipe/build.sh
@@ -37,10 +37,9 @@ fi
 OPENBLAS_NUM_THREADS=$CPU_COUNT make test
 make install PREFIX="${PREFIX}"
 
-# As OpenBLAS, now will have all symbols that BLAS or LAPACK have,
+# As OpenBLAS, now will have all symbols that BLAS, CBLAS or LAPACK have,
 # create libraries with the standard names that are linked back to
 # OpenBLAS. This will make it easier for packages that are looking for them.
-
 for arg in blas cblas lapack; do
     ln -fs $PREFIX/lib/pkgconfig/openblas.pc $PREFIX/lib/pkgconfig/$arg.pc
     ln -fs $PREFIX/lib/libopenblas.a $PREFIX/lib/lib$arg.a
