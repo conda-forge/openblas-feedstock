dea9c0491865641df87d46751b011a9dfb89c7d2
Isuru Fernando <isuruf@gmail.com>
2017-07-30 00:51:24 +0530

    MNT: Re-rendered with conda-smithy 2.3.2

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,10 @@
 mkdir build
 cd build
-cmake -G "Ninja" -DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_C_COMPILER=clang-cl -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DARCH=x86_64 ..
+if "%ARCH" == "64" (
+    set OPENBLAS_ARCH=x86_64
+) ELSE (
+    set OPENBLAS_ARCH=x86
+)
+cmake -G "Ninja" -DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_C_COMPILER=clang-cl -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DARCH=%OPENBLAS_ARCH% ..
 cmake --build . --target install
 utest\openblas_utest.exe
