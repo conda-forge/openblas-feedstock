e0823aea9d2c50985b171dcbb3386dac92c6ee9e
Isuru Fernando <isuruf@gmail.com>
2017-07-29 22:53:30 +0530

    Set ARCH

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 mkdir build
 cd build
-cmake -G "Ninja" -DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_C_COMPILER=clang-cl -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
+cmake -G "Ninja" -DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_C_COMPILER=clang-cl -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DARCH=x86_64 ..
 cmake --build . --target install
 utest\openblas_utest.exe
