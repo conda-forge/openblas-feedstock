0d69642abbf0f1193ee5d9923374e295fdc5f976
Isuru Fernando <isuruf@gmail.com>
2017-07-30 11:28:36 +0530

    Use CLAPACK

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,3 +8,19 @@ if "%ARCH%" == "64" (
 cmake -G "Ninja" -DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_C_COMPILER=clang-cl -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DARCH=%OPENBLAS_ARCH% ..
 cmake --build . --target install
 utest\openblas_utest.exe
+
+mkdir %SRC_DIR%\clapack
+cd %SRC_DIR%\clapack
+curl -fsS -o clapack-3.2.1-CMAKE.tgz http://uvcdat.llnl.gov/cdat/resources/clapack-3.2.1-CMAKE.tgz
+7z x clapack-3.2.1-CMAKE.tgz
+7z x clapack-3.2.1-CMAKE.tar
+
+cd clapack-3.2.1
+rm BLAS/CMakeLists.txt
+cp %RECIPE_DIR%/clapack-blas-CMakeLists.txt BLAS/CMakeLists.txt
+
+mkdir build
+cd build
+cmake -G "Ninja" -DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_C_COMPILER=clang-cl -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
+cmake --build . --target install
+
