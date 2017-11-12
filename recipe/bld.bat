c28808ba842fe3ce0a8a2578b8ea0028ea8e8609
xoviat <xoviat@users.noreply.github.com>
2017-11-12 17:48:27 -0600

    [bld.bat] update cmake parameters

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,13 +5,13 @@ if "%ARCH%" == "64" (
 ) ELSE (
     set OPENBLAS_ARCH=x86
 )
-cmake -G "Ninja" ^
-    -DCMAKE_CXX_COMPILER=clang-cl           ^
-    -DCMAKE_C_COMPILER=clang-cl             ^
-    -DCMAKE_Fortran_COMPILER=flang          ^
-    -DCMAKE_BUILD_TYPE=Release              ^
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -DARCH=%OPENBLAS_ARCH%                  ^
+cmake -G "NMake Makefiles" ^
+    -DCMAKE_CXX_COMPILER=clang-cl ^
+    -DCMAKE_C_COMPILER=clang-cl ^
+    -DCMAKE_Fortran_COMPILER=flang ^
+    -DBUILD_WITHOUT_LAPACK=no ^
+    -DNOFORTRAN=0 ^
+    -DDYNAMIC_ARCH=ON ^
     ..
 
 cmake --build . --target install -- -j%CPU_COUNT%
