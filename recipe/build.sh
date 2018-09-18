f491f6eb023caa915cb873a46afa7a53c9c97ee2
Marius van Niekerk <marius.v.niekerk@gmail.com>
2018-09-18 15:32:17 -0400

    disable openmp (GNU openmp not fork safe; causing hangs)
    cherry-picked from https://github.com/AnacondaRecipes/openblas-feedstock/commit/2bac9afd5d1aafd7ff6e3fe7c6ec0b7800eeb2b4
    
    # Conflicts:
    #	recipe/build.sh
    #	recipe/meta.yaml

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,18 +10,31 @@ unset CFLAGS
 
 if [[ `uname` == 'Darwin' ]]; then
      export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
+     USE_OPENMP="0"
 else
      export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
+    # Gnu OpenMP is not fork-safe.  We disable openmp right now, so that downstream packages don't hang as a result of this.
+    # USE_OPENMP="1"
+    USE_OPENMP="0"
 fi
 eval export ${LIBRARY_SEARCH_VAR}="${PREFIX}/lib"
+
+
+# USE_SIMPLE_THREADED_LEVEL3 is necessary to avoid hangs when more than one process uses blas:
+#    https://github.com/xianyi/OpenBLAS/issues/1456
+#    https://github.com/xianyi/OpenBLAS/issues/294
+#    https://github.com/scikit-learn/scikit-learn/issues/636
+
 
 # Build all CPU targets and allow dynamic configuration
 # Build LAPACK.
 # Enable threading. This can be controlled to a certain number by
 # setting OPENBLAS_NUM_THREADS before loading the library.
-make DYNAMIC_ARCH=1 BINARY=${ARCH} NO_LAPACK=0 NO_AFFINITY=1 USE_THREAD=1 NUM_THREADS=128 CFLAGS="${CF}" FFLAGS="-frecursive"
+make DYNAMIC_ARCH=1 BINARY=${ARCH} NO_LAPACK=0 NO_AFFINITY=1 USE_THREAD=1 NUM_THREADS=128 \
+     USE_OPENMP="${USE_OPENMP}" USE_SIMPLE_THREADED_LEVEL3=1 CFLAGS="${CF}" FFLAGS="-frecursive"
 OPENBLAS_NUM_THREADS=$CPU_COUNT make test
 make install PREFIX="${PREFIX}"
+
 
 # As OpenBLAS, now will have all symbols that BLAS, CBLAS or LAPACK have,
 # create libraries with the standard names that are linked back to
