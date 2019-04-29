0d1073bf247a4b46f3559fbcf7d7a5dfa6de105e
Isuru Fernando <isuruf@gmail.com>
2019-04-29 13:50:51 -0500

    Remove blas, cblas, lapack symlinks

--- recipe/build.sh
+++ recipe/build.sh
@@ -49,20 +49,11 @@ make DYNAMIC_ARCH=1 BINARY=${ARCH} NO_LAPACK=0 NO_AFFINITY=1 USE_THREAD=1 NUM_TH
 OPENBLAS_NUM_THREADS="${CPU_COUNT}" CFLAGS="${CF}" FFLAGS="${FFLAGS} -frecursive" make test
 make install PREFIX="${PREFIX}"
 
-
-# As OpenBLAS, now will have all symbols that BLAS, CBLAS or LAPACK have,
-# create libraries with the standard names that are linked back to
-# OpenBLAS. This will make it easier for packages that are looking for them.
-for arg in blas cblas lapack; do
-    ln -fs $PREFIX/lib/pkgconfig/openblas.pc $PREFIX/lib/pkgconfig/$arg.pc
-    ln -fs $PREFIX/lib/libopenblas.a $PREFIX/lib/lib$arg.a
-    ln -fs $PREFIX/lib/libopenblas$SHLIB_EXT $PREFIX/lib/lib$arg$SHLIB_EXT
-done
-
 if [[ `uname` == 'Darwin' ]]; then
     # Needs to fix the install name of the dylib so that the downstream projects will link
     # to libopenblas.dylib instead of libopenblasp-r0.2.20.dylib
     # In linux, SONAME is libopenblas.so.0 instead of libopenblasp-r0.2.20.so, so no change needed
-    install_name_tool -id ${PREFIX}/lib/libopenblas.dylib ${PREFIX}/lib/libopenblas.dylib;
+    ln -s ${PREFIX}/lib/libopenblas.dylib ${PREFIX}/lib/libopenblas.0.dylib
+    install_name_tool -id ${PREFIX}/lib/libopenblas.0.dylib ${PREFIX}/lib/libopenblas.dylib;
 fi
 
