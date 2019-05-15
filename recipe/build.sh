9e9057516a06f2e6e7795c44dfc8eb21b29ecf94
Sylvain Corlay <sylvain.corlay@gmail.com>
2019-05-15 20:52:37 +0200

    Add .so symlinks

--- recipe/build.sh
+++ recipe/build.sh
@@ -49,6 +49,15 @@ make QUIET_MAKE=1 DYNAMIC_ARCH=1 BINARY=${ARCH} NO_LAPACK=0 NO_AFFINITY=1 USE_TH
 OPENBLAS_NUM_THREADS="${CPU_COUNT}" CFLAGS="${CF}" FFLAGS="${FFLAGS} -frecursive" make test
 make install PREFIX="${PREFIX}"
 
+# As OpenBLAS, now will have all symbols that BLAS, CBLAS or LAPACK have,	
+# create libraries with the standard names that are linked back to	
+# OpenBLAS. This will make it easier for packages that are looking for them.	
+for arg in blas cblas lapack; do	
+    ln -fs $PREFIX/lib/pkgconfig/openblas.pc $PREFIX/lib/pkgconfig/$arg.pc	
+    ln -fs $PREFIX/lib/libopenblas.a $PREFIX/lib/lib$arg.a	
+    ln -fs $PREFIX/lib/libopenblas$SHLIB_EXT $PREFIX/lib/lib$arg$SHLIB_EXT	
+done
+
 if [[ `uname` == 'Darwin' ]]; then
     # Needs to fix the install name of the dylib so that the downstream projects will link
     # to libopenblas.dylib instead of libopenblasp-r0.2.20.dylib
