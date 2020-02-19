1a11c675d3b79a5fb2cc2c6e118586192972aa6c
Isuru Fernando <isuruf@gmail.com>
2020-02-19 11:30:35 -0600

    Simplify stuff

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-# https://github.com/xianyi/OpenBLAS/wiki/faq#Linux_SEGFAULT
-patch < segfaults.patch
-
-# See this workaround
-# ( https://github.com/xianyi/OpenBLAS/issues/818#issuecomment-207365134 ).
-CF="${CFLAGS}"
-unset CFLAGS
-
 # Fix ctest not automatically discovering tests
 LDFLAGS=$(echo "${LDFLAGS}" | sed "s/-Wl,--gc-sections//g")
 
 # silly if statement but makes things clear
 if [[ "${target_platform}" == "osx-64" ]]; then
     USE_OPENMP="1"
-elif [[ "${target_platform}" == "linux-aarch64" ]]; then
-    # Gnu OpenMP is not fork-safe and we do not have llvm OpenMP
-    USE_OPENMP="0"
-else
+elif [[ "${target_platform}" == linux-* ]]; then
     # We will have to build with GNU and then use LLVM on run
     USE_OPENMP="1"
 fi
@@ -55,6 +44,6 @@ fi
 # setting OPENBLAS_NUM_THREADS before loading the library.
 # Tests are run as part of build
 make QUIET_MAKE=1 DYNAMIC_ARCH=1 BINARY=${ARCH} NO_LAPACK=0 NO_AFFINITY=1 USE_THREAD=1 NUM_THREADS=128 \
-     USE_OPENMP="${USE_OPENMP}" USE_SIMPLE_THREADED_LEVEL3=1 CFLAGS="${CF}" FFLAGS="${FFLAGS} -frecursive" \
+     USE_OPENMP="${USE_OPENMP}" CFLAGS="${CFLAGS}" FFLAGS="${FFLAGS} -frecursive" \
      INTERFACE64=${INTERFACE64} SYMBOLSUFFIX=${SYMBOLSUFFIX} HOST=${HOST} $TARGET CROSS_SUFFIX="${HOST}-"
 make install PREFIX="${PREFIX}"
