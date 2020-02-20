9190102131b080cb3ac62494428ca47509a6b896
Isuru Fernando <isuruf@gmail.com>
2020-02-20 01:42:40 +0000

    Set CFLAGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,11 @@
 # Fix ctest not automatically discovering tests
 LDFLAGS=$(echo "${LDFLAGS}" | sed "s/-Wl,--gc-sections//g")
 
+# See this workaround
+# ( https://github.com/xianyi/OpenBLAS/issues/818#issuecomment-207365134 ).
+CF="${CFLAGS}"
+unset CFLAGS
+
 # silly if statement but makes things clear
 if [[ "${target_platform}" == "osx-64" ]]; then
     USE_OPENMP="1"
@@ -25,11 +30,6 @@ export FFLAGS="${FFLAGS} -frecursive"
 [[ -d "${PREFIX}"/include ]] || mkdir "${PREFIX}"/include
 [[ -d "${PREFIX}"/lib ]] || mkdir "${PREFIX}"/lib
 
-# USE_SIMPLE_THREADED_LEVEL3 is necessary to avoid hangs when more than one process uses blas:
-#    https://github.com/xianyi/OpenBLAS/issues/1456
-#    https://github.com/xianyi/OpenBLAS/issues/294
-#    https://github.com/scikit-learn/scikit-learn/issues/636
-
 # Set CPU Target
 if [[ "${target_platform}" == linux-aarch64 ]]; then
   TARGET="ARMV8"
@@ -47,8 +47,8 @@ fi
 # Enable threading. This can be controlled to a certain number by
 # setting OPENBLAS_NUM_THREADS before loading the library.
 # Tests are run as part of build
-make QUIET_MAKE=1 DYNAMIC_ARCH=1 BINARY=${BINARY} NO_LAPACK=0 \
-     HOST=${HOST} TARGET=${TARGET} CROSS_SUFFIX="${HOST}-"
+make DYNAMIC_ARCH=1 BINARY=${BINARY} NO_LAPACK=0 CFLAGS="${CF}" \
+     HOST=${HOST} TARGET=${TARGET} CROSS_SUFFIX="${HOST}-" \
      NO_AFFINITY=1 USE_THREAD=1 NUM_THREADS=128 USE_OPENMP="${USE_OPENMP}" \
-     INTERFACE64=${INTERFACE64} SYMBOLSUFFIX=${SYMBOLSUFFIX} \
+     INTERFACE64=${INTERFACE64} SYMBOLSUFFIX=${SYMBOLSUFFIX}
 make install PREFIX="${PREFIX}"
